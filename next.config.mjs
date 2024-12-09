591e2bb3b44aaafaffbd5db93f928ee526cfeabe
Dayeeta <dayeetaganguly@yahoo.com>
2024-12-10 00:47:08 +0530

    Github pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  basePath: "/admin_ui_nextjs",
-  output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true, 
+  },
+  assetPrefix: isProd ? '/admin_ui_nextjs/' : '',
+  basePath: isProd ? '/admin_ui_nextjs' : '',
+  output: 'export'
 };
 
 export default nextConfig;
